7ec267ad36c5b81842bb5edda821d0a6957f4e73
Yoimer <ydrf19@gmail.com>
2019-07-01 15:27:47 -0430

    added templatesm in flight

--- djangoair-v0/flights/views.py
+++ djangoair-v0/flights/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# from this current models module - package
+from .models import Flight
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to DjangoAir!")
+    # return HttpResponse("Welcome to DjangoAir!")
+    # render takes a couple of arguments, the request and the html template, context
+    context = {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
