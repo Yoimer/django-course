9b0344bdcff5c6b033069a9a68c6935b84a1a490
Yoimer <ydrf19@gmail.com>
2019-07-01 22:45:13 -0430

    added superuser administrator and flight on template

--- djangoair-v0/flights/admin.py
+++ djangoair-v0/flights/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Airport, Flight
+
 # Register your models here.
+# admin is an app built-in on djando
+# and makes very easy to modify and update data in existing models
+# what I am doing here is, making admin able to manipulate Airport and Flight
+admin.site.register(Airport)
+admin.site.register(Flight)
--- djangoair-v0/flights/views.py
+++ djangoair-v0/flights/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 # from this current models module - package
@@ -12,3 +12,13 @@ def index(request):
         "flights": Flight.objects.all()
     }
     return render(request, "flights/index.html", context)
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight does not exist.")
+    context = {
+        "flight": flight
+    }
+    return render(request, "flights/flight.html", context)
